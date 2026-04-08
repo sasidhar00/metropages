7bd8de4bcc810763b3773dc9fd46f32be67519de
sasidhar00 <42925917+sasidhar00@users.noreply.github.com>
2026-04-08 06:12:02 +0530

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,8 @@ import (
 	"log"
 	"strings"
 	"time"
-
+    "os"
+	"libsql"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
